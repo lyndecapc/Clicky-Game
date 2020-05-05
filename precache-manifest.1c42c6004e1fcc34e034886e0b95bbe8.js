095863709362b51e8075f6bfcfe0191ba9eb2fce
Lynde Capogreco <lyndecapc@gmail.com>
2020-05-05 10:10:33 -0500

    Updates

---
+++ precache-manifest.1c42c6004e1fcc34e034886e0b95bbe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02e58b900df839d92d626b6894d62785",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "f5993bd13eea8875e3d6",
+    "url": "/Clicky-Game/static/css/main.28001855.chunk.css"
+  },
+  {
+    "revision": "25edf8911c86ff417e68",
+    "url": "/Clicky-Game/static/js/2.666e6ab3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.666e6ab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5993bd13eea8875e3d6",
+    "url": "/Clicky-Game/static/js/main.edda9bf7.chunk.js"
+  },
+  {
+    "revision": "9afdf8f507032edb2a0c",
+    "url": "/Clicky-Game/static/js/runtime-main.c78aa30a.js"
+  }
+]);
